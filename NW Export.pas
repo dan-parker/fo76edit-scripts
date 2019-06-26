f259f54061a9e1fa640b716f431701d4c2834416
Dan Parker <dan.parker.or@gmail.com>
2019-06-25 18:42:38 -0700

    Skip Disabled items, add additional terminals, powerarmor, safes

--- NW Export.pas
+++ NW Export.pas
@@ -24,24 +24,163 @@ var
 begin
   if Signature(e) = 'REFR' then begin
 
+  //NW Mode disables some of the placed items from normal game, let's skip those..
+  if GetIsDeleted(e) then exit;
+
    edid := BaseName(e);
-   if (pos('LPI_Babylon_Loot_Magazines',edid)>0) then begin
+   if (pos('LootPriorityPrewar_Safe',edid)>0) then begin
 	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
-	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Magazine",';
-	Row := Row +  '"type":"MagazineMarker",';
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Safe",';
+	Row := Row +  '"type":"SafeMarker",';
 	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
 	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
 	sl.Add(row);
    end;
-   if (pos('LPI_Babylon_Loot_Bobbleheads',edid)>0) then begin
-	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
-	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Bobblehead",';
-	Row := Row +  '"type":"BobbleMarker",';
-	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
-	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
-	sl.Add(row);
-   end;
+//   if (pos('LPI_Babylon_Loot_Magazines',edid)>0) then begin
+//	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+//	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Magazine",';
+//	Row := Row +  '"type":"MagazineMarker",';
+//	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+//	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+//	sl.Add(row);
+//   end;
+//   if (pos('LPI_Babylon_Loot_Bobbleheads',edid)>0) then begin
+//	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+//	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Bobblehead",';
+//	Row := Row +  '"type":"BobbleMarker",';
+//	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+//	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+//	sl.Add(row);
+ //  end;
    if (pos('Babylon_Loot_Terminal',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('RSVP00_Terminal',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('NewRiverGorgeBridge_Terminal',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('LC130_KeeperTerminal',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('Helvetia_Lore_BallotTerminal',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('POI088_SuttonTrainStationTerminal',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('LC167Arktos',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('LC167_Arktos',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('EN06_RegistrationTerminal',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('DefaultTerminalWall',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('EN05_MarksmanshipCourseTerminal',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('EN05_Patriotism',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('EN05_ObstacleCourseTerminal',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('EN05_CombatTerminal',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('LC118_DrillInstructorTerminal',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('v63CertificationMilitaryTerminal',edid)>0) then begin
+	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
+	Row := Row +  '"type":"TerminalMarker",';
+	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+	sl.Add(row);
+   end;
+   if (pos('FF08_LoreBeanstalkQuery',edid)>0) then begin
 	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
 	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Terminal",';
 	Row := Row +  '"type":"TerminalMarker",';
@@ -58,11 +197,11 @@ begin
 	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
 	sl.Add(row);
    end;
-   if (pos('LPI_Babylon_RadiationBarrels',edid)>0) then begin
+   if (pos('LPI_Babylon_LvlPowerArmorFurniture',edid)>0) then begin
 	if (FixedFormID(e) = 5145111) then exit; //Skip offmap item
 	if (pos('\[00] SeventySix.esm\[70] Cell\',PathName(e))>0) then exit; //Skip Internal Cells
-	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Radiation Barrel",';
-	Row := Row +  '"type":"BarrelMarker",';
+	Row := '{"id":'+IntToStr(FixedFormID(e))+',"name":"Power Armor",';
+	Row := Row +  '"type":"PAMarker",';
 	Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
 	Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
 	sl.Add(row);
