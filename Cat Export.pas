9913b15a978bd15906c26e93f0b8361d3a74a4ab
Dan Parker <dan.parker@firstcallres.com>
2019-09-11 16:33:41 -0700

    Initial Cat Export
    Example export template

---
+++ Cat Export.pas
@@ -0,0 +1,75 @@
+{
+  Exports Cat Spawn data to JSON
+  Intended for Fallout 76
+}
+unit userscript;
+
+var
+  sl: TStringList;
+
+procedure GetMarkers;
+var
+  wrld, wrldgrup, block, subblock, cell, e: IInterface;
+  i,w,x,y,z,counter: integer;
+  row, Name: string;
+begin
+    //Let's try to filter to the specific worldspace so we don't have to search through more stuff...
+    if wbGameMode = gmFNV then
+      wrld := RecordByFormID(FileByIndex(0), $000DA726, False)
+    else if wbGameMode = gmFO76 then
+      wrld := RecordByFormID(FileByIndex(0), 2480661, False)
+    else
+      wrld := RecordByFormID(FileByIndex(0), $0000003C, False);
+
+    wrldgrup := ChildGroup(wrld);
+     for i := 0 to ElementCount(wrldgrup) - 1 do begin
+	block := ElementByIndex(wrldgrup,i);
+	//AddMessage('Block '+BaseName(Block));
+	for x := 0 to ElementCount(block) -1 do begin
+	   subblock := ElementByIndex(block,x);
+	  //AddMessage('Block '+BaseName(block) +' SubBlock '+BaseName(subblock));
+	   	for y := 0 to ElementCount(subblock) -1 do begin
+			//We only want to look through Temp items
+			cell := FindChildGroup(ChildGroup(ElementByIndex(subblock,y)),9,ElementByIndex(subblock,y));
+			for z := 0 to ElementCount(cell) -1 do begin
+				e := ElementByIndex(cell,z);
+				Name := GetEditValue(ElementByName(e,'NAME - Base'));
+				if (pos('LvlCritterCat',Name) > 0) then begin
+					Row := '{"id":"'+IntToHex(FixedFormID(e), 8)+'","name":"'+Name+'",';
+					Row := Row +  '"type":"CatMarker",';
+					Row := Row +  '"x":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'X'))+',';
+					Row := Row +  '"y":'+GetEditValue(ElementByName(ElementByName(ElementByName(e,'DATA - Position/Rotation'),'Position'),'Y'))+'},';
+					sl.Add(row);
+				end;
+			end;
+		end;
+	end;
+     end;
+end;
+
+
+// Called before processing
+// You can remove it if script doesn't require initialization code
+function Initialize: integer;
+begin
+    sl := TStringList.Create;
+    sl.Add('[');
+    GetMarkers;
+    Result := 0;
+end;
+
+function Finalize: integer;
+var
+  fname: string;
+begin
+  fname := ProgramPath + 'Cat.json';
+  //Dummy record for trailing comma
+  sl.Add('{"id":9999999,"name":"","type":"","x":0,"y":0}');
+  sl.Add(']');
+  sl.SaveToFile(fname);
+  sl.Free;
+  Result := 1;
+end;
+
+
+end.
